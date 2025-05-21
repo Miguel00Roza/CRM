8fae6edbd39c876ecb8fc68e99d97c92528de3f7
Miguel00Roza <miguelroza13@gmail.com>
2025-05-20 23:38:10 -0300

    teste de rotas, as alterações desse commit serao apagadas no futuro

--- app/views.py
+++ app/views.py
@@ -1,5 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, jsonify
+import random
+
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    context = {
+        'usuario': 'Miguel',
+        'idade': 15,
+        'cargo': 'estudante'
+    }
+
+    return render_template('index.html', context=context)
+
+@app.route('/new')
+def nova():
+    return 'teste de pagina'
